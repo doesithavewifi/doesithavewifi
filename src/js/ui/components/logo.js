b49caec476f7845f1602df02d7bae90886bda7c3
Ramesh Nair <ram@hiddentao.com>
2015-06-29 14:54:12 +0800

    ionicons added

--- src/js/ui/components/logo.js
+++ src/js/ui/components/logo.js
@@ -4,9 +4,8 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {    
     return (
-      <div>
+      <div className="logo">
         "Ahhhnold!"
-        <i className="fa fa-gear fa-2x" />
       </div>
     );    
   },
