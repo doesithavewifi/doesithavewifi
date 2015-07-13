48e4c8f4ab4e5af1b6e3fb22f84b852c2110d2c2
Ramesh Nair <ram@hiddentao.com>
2015-07-13 16:14:35 +0800

    added spinning loader

---
+++ src/js/ui/components/loader.js
@@ -0,0 +1,45 @@
+var React = require('react');
+
+var SpinLoader = require('react-loader');
+
+
+
+module.exports = React.createClass({
+  propTypes: {
+    text : React.PropTypes.string,
+  },
+
+  getDefaultProps: function() {
+    return {
+      text: null,
+    };
+  },
+
+  render: function() {  
+    var options = {
+      lines: 13,
+      length: 20,
+      width: 10,
+      radius: 30,
+      corners: 1,
+      rotate: 0,
+      direction: 1,
+      color: '#000',
+      speed: 1,
+      trail: 60,
+      shadow: false,
+      hwaccel: false,
+      zIndex: 2e9,
+      top: '50%',
+      left: '50%',
+      scale: 3.00
+    };
+
+    return (
+      <SpinLoader className="loader" loaded={false} {...options}>
+        {this.props.text}
+      </SpinLoader>
+    );
+  },
+
+});
