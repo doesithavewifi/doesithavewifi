2f953098dcf6f88429b14754cc20b6c17151ca0c
Ramesh Nair <ram@hiddentao.com>
2015-08-03 17:59:34 +0800

    static map fix

---
+++ src/js/ui/components/staticMap.js
@@ -0,0 +1,29 @@
+var React = require('react');
+var Link = require('react-router').Link;
+
+
+
+module.exports = React.createClass({
+  propTypes: {
+    lat : React.PropTypes.number.isRequired,
+    lng : React.PropTypes.number.isRequired,
+  },
+
+  render: function() {
+    let latlng = this.props.lat + ',' + this.props.lng;
+
+    let url = 'http://maps.googleapis.com/maps/api/staticmap?'
+      + 'center=' + latlng
+      + '&size=600x300'
+      + '&scale=2'
+      + '&zoom=14'
+      + '&format=png'
+      + '&maptype=roadmap'
+      + '&markers=color:red|' + latlng
+    ;
+
+    return (
+      <img className="static-map" onClick={this._openExternalMap} src={url} />
+    );
+  }
+});
