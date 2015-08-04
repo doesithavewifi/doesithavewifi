ff3f3f73da637fe4a590d76c185f881190268329
Ramesh Nair <ram@hiddentao.com>
2015-08-04 14:57:58 +0800

    update about popup text

--- src/js/ui/components/about.js
+++ src/js/ui/components/about.js
@@ -17,7 +17,10 @@ module.exports = React.createClass({
         <div className="about-popup">
           <h2>About this project</h2>
           <p>
-            Does it have Wifi is a project to document all the working cafes in a city. The team behind Does it have wifi are all based in Taiwan and predominantly work from home or cafes, but finding cafes with the right environment for working is difficult. Does it have wifi makes it easy for you to find those cafes
+            Does it have Wifi is a project to document all the working cafes in a city. The team behind Does it have wifi are all based in Taiwan and predominantly work from home or cafes, but finding cafes with the right environment for working is difficult. Does it have wifi makes it easy for you to find those cafes.
+          </p>
+          <p>
+            By <a href="https://twitter.com/Jef_Lau">@Jef_Lau</a>, <a href="https://twitter.com/hiddentao">@hiddentao</a> and <a href="https://twitter.com/leemanrolls">@leemanrolls</a>.
           </p>
         </div>
         <div className={classesOverlay} onClick={this.props.toggleAbout}></div>
