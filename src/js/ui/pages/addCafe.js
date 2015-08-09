073e722c5b77fbd339e6519818ec5ea798de6f88
Ramesh Nair <ram@hiddentao.com>
2015-08-09 23:37:35 +0800

    add cafe form

--- src/js/ui/pages/addCafe.js
+++ src/js/ui/pages/addCafe.js
@@ -6,7 +6,7 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div>Add form here...</div>
+      <iframe src="https://docs.google.com/forms/d/1NEJwKLDHUh2IEgIoWFgx8bvv52fqi9KfZ4NfdW5mAF8/viewform?embedded=true#start=embed" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
     );
   },
 });
